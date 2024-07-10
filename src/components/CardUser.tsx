f52be460cd9f938b1d3809777ac6dffaccc2ae27
Jumakri Ridho Fauzi <ridhoajibx@gmail.com>
2024-07-10 17:04:02 +0700

    [ridho] - create component modal

--- src/components/CardUser.tsx
+++ src/components/CardUser.tsx
@@ -1,5 +1,6 @@
 import { Avatar, Box, Card, CardContent, CardHeader, Typography } from '@mui/material'
-import { FC } from 'react'
+import { FC, Fragment, useState } from 'react'
+import ModalComponent from './common/ModalComponent';
 
 type Users = {
   avatar?: string;
@@ -14,35 +15,62 @@ type Users = {
 // };
 
 export const CardUser: FC<Users> = ({ avatar, name, occupation, description }) => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const onOpenCard = () => {
+    setIsOpen(true)
+  }
+
+  const onCloseCard = () => {
+    setIsOpen(false)
+  }
+
   return (
-    <Card
-      sx={{
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'space-between',
-        flexGrow: 1,
-        p: 1,
-      }}
-    >
-      <Box
+    <Fragment>
+      <Card
         sx={{
           display: 'flex',
-          flexDirection: 'row',
+          flexDirection: 'column',
           justifyContent: 'space-between',
-          pr: 2,
+          flexGrow: 1,
+          p: 1,
+          ":hover": {
+            cursor: "pointer"
+          }
         }}
+        onClick={onOpenCard}
       >
-        <CardHeader
-          avatar={<Avatar alt={name} src={avatar} />}
-          title={name}
-          subheader={occupation}
-        />
-      </Box>
-      <CardContent>
-        <Typography variant="body2" color="text.secondary">
-          {description}
-        </Typography>
-      </CardContent>
-    </Card>
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            pr: 2,
+          }}
+        >
+          <CardHeader
+            avatar={<Avatar alt={name} src={avatar} />}
+            title={name}
+            subheader={occupation}
+          />
+        </Box>
+        <CardContent>
+          <Typography variant="body2" color="text.secondary">
+            {description}
+          </Typography>
+        </CardContent>
+      </Card>
+
+      <ModalComponent isOpen={isOpen} handleClose={onCloseCard} size='md' position='center'>
+        <div>
+          <Typography id="transition-modal-title" variant="h6" component="h2">
+            Text in a modal
+          </Typography>
+          <Typography id="transition-modal-description" sx={{ mt: 2 }}>
+            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+          </Typography>
+        </div>
+      </ModalComponent>
+    </Fragment>
   )
 }
---
+++ src/components/common/ModalComponent.tsx
@@ -0,0 +1,95 @@
+import Backdrop from '@mui/material/Backdrop';
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
+import Fade from '@mui/material/Fade';
+import { FC, ReactNode, useMemo } from 'react';
+
+export type ModalProps = {
+  children?: ReactNode;
+  isOpen: boolean;
+  handleClose: () => void;
+  size?: "sm" | "md" | "lg";
+  position?: "top" | "bottom" | "center";
+}
+
+const ModalComponent: FC<ModalProps> = ({ isOpen, handleClose, children, size, position }) => {
+  // size
+  const sizes = useMemo(() => {
+    const _size = size || "sm";
+    let _sizeR: number | string = 400;
+    switch (_size) {
+      case "sm":
+        _sizeR;
+        break;
+      case "md":
+        _sizeR = 750;
+        break;
+      case "lg":
+        _sizeR = 1000;
+        break;
+      default:
+        _sizeR;
+        break;
+    }
+    return _sizeR
+  }, [size])
+
+  // position
+  const positions = useMemo(() => {
+    const _pos = position || "center";
+    let top: number | string = '50%'
+    switch (_pos) {
+      case "center":
+        top;
+        break;
+      case "top":
+        top = "10%";
+        break;
+      case "bottom":
+        top = "90%";
+        break;
+      default:
+        top;
+        break;
+    }
+    return top
+  }, [position])
+
+  // global-styles
+  const styles = useMemo(() => {
+    const style = {
+      position: 'absolute' as const,
+      top: positions,
+      left: '50%',
+      transform: 'translate(-50%, -50%)',
+      width: sizes,
+      bgcolor: 'background.paper',
+      p: 2
+    };
+    return style;
+  }, [sizes, positions])
+
+  return (
+    <Modal
+      aria-labelledby="transition-modal-title"
+      aria-describedby="transition-modal-description"
+      open={isOpen}
+      onClose={handleClose}
+      closeAfterTransition
+      slots={{ backdrop: Backdrop }}
+      slotProps={{
+        backdrop: {
+          timeout: 500,
+        },
+      }}
+    >
+      <Fade in={isOpen}>
+        <Box sx={styles}>
+          {children}
+        </Box>
+      </Fade>
+    </Modal>
+  );
+};
+
+export default ModalComponent;
